000da52200e82e591a0c48f1360c1eeed32d7d9e
Daniel Anca <steptu94@gmail.com>
2022-07-12 14:19:02 +0300

    Update for SSR

---
+++ src/services/routes.cjs
@@ -0,0 +1,27 @@
+// import ProduseleNoastre from "../components/ProduseleNoastre";
+// import MainNavigation from "../Navigation/MainNavigation";
+// const ProduseleNoastre = require("../components/ProduseleNoastre");
+// require("../components/MainNavigation").MainNavigation;
+import React from "react";
+// const routes = {
+//   "/": { path: "/", exact: true, component: <div>blyat</div> },
+
+//   "/produsele-noastre": {
+//     path: "/produsele-noastre",
+//     component: <div>blyasst</div>
+//   }
+// };
+const routes = [
+  { path: "/", exact: true, component: <div>blyat</div> },
+
+  { path: "/produsele-noastre", component: <div>blyasst</div> }
+];
+//   "/": { path: "/", exact: true, component: <div>blyat</div> },
+
+//   "/produsele-noastre": {
+//     path: "/produsele-noastre",
+//     component: <div>blyasst</div>
+//   }
+// };
+
+export default routes;
