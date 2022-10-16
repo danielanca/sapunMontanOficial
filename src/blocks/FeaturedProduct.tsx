cdb9c81c95226fb175dc551fc57aaeb7bcd2eab0
Daniel Anca <steptu94@gmail.com>
2022-10-16 16:14:27 +0300

    no message

--- src/blocks/FeaturedProduct.tsx
+++ src/blocks/FeaturedProduct.tsx
@@ -5,25 +5,45 @@ import ButtonNice from "../components/MiniComponents/Products/ButtonNice";
 import styles from "./FeaturedProduct.module.scss";
 import images from "../data/images";
 
-const FeaturedProduct = () => {
+interface featuredDataModel {
+  bigAnnouncement: {
+    background: string;
+    title: string;
+  };
+  subTitle: string;
+  title: string;
+  text: string;
+  featureThings: {
+    [key: string]: {
+      image: string;
+      title: string;
+      text: string;
+    };
+  };
+  actionButton: {
+    textButton: string;
+    link: string;
+  };
+}
+
+interface contentProps {
+  content: featuredDataModel;
+}
+const FeaturedProduct = ({ content }: contentProps) => {
   return (
     <div className={styles.featuredContainer}>
-      <FeaturedText text={{ title: "MontanAir", textSmall: "Produs Nou" }} />
+      <FeaturedText text={{ title: content.bigAnnouncement.background, textSmall: content.bigAnnouncement.title }} />
       <div className={styles.bigPicture}>
         <div className={styles.roundCircle}>
           <img className={styles.featuredStyle} src={images.featuredProduct} />
         </div>
-        <h3 className={styles.topTextFeatured}>{"MASCA NUTRITIONALA"}</h3>
-        <h3 className={styles.bottomTextFeatured}>{"REPARATOARE SI ANTI-PETE"}</h3>
+        <h3 className={styles.topTextFeatured}>{content.title}</h3>
+        <h3 className={styles.bottomTextFeatured}>{content.subTitle}</h3>
         <div className={styles.textDescription}>
-          <p>
-            {
-              "Oferă un efect vizibil de hidratare și reparare a pielii sensibile, uscate, predispusă la coșuri, acnee și puncte negre."
-            }
-          </p>
+          <p>{content.text}</p>
         </div>
         <Ingredients />
-        <ButtonNice title="VEZI PRODUSUL" urlSufix="piutzyke" />
+        <ButtonNice title={content.actionButton.textButton} urlSufix={content.actionButton.link} />
       </div>
     </div>
   );
