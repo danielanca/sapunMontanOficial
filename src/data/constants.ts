6b4722992cb8a5362edb471dfa3ed58729abc183
Daniel Anca <steptu94@gmail.com>
2022-11-11 23:44:48 +0200

    New types migrated to files and strings

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -2,3 +2,21 @@ export const CartInfoItemCookie: string = "cartData";
 export const ProductsFromSessionStorage: string = "productsFetched";
 export const CookiesTagConsent = "cookieConsentBrasov";
 export const userAcceptedCookies = "userAccepted";
+
+//Newsletter Component
+
+export const inputStateEmail = {
+  valid: "valid",
+  notValid: "notValid",
+  init: "init"
+};
+export type emailValidType =
+  | typeof inputStateEmail.init
+  | typeof inputStateEmail.notValid
+  | typeof inputStateEmail.valid;
+
+//Newsletter Subscrption state
+
+export const Sub = { initState: "INIT", SubscribedState: "SUBSCRIBED", ErrorState: "ERROR" };
+
+export type SubscriptionType = typeof Sub.initState | typeof Sub.SubscribedState | typeof Sub.ErrorState;
