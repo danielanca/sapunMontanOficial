c59dbde5effd54399ae8c48d9080f3a65f07d550
Daniel Anca <steptu94@gmail.com>
2022-11-13 14:13:30 +0200

    Fixed different bugs related to FeaturedProducts

--- src/components/MiniComponents/Products/Ingredients.tsx
+++ src/components/MiniComponents/Products/Ingredients.tsx
@@ -1,15 +1,25 @@
+import React from "react";
 import parse from "html-react-parser";
 import styles from "./Ingredients.module.scss";
-import images from "./../../../data/images";
-import strings from "../../../data/strings.json";
 
-const Ingredients = () => {
+interface ingredientsProps {
+  [key: string]: {
+    title: string;
+    text: string;
+    image: string;
+  };
+}
+interface Proper {
+  contentIngredients: ingredientsProps;
+}
+const Ingredients = ({ contentIngredients }: Proper) => {
   return (
     <div className={styles.ingredientsContainer}>
-      {Object.values(strings.productDescription).map((item) => (
+      {Object.values(contentIngredients).map((item) => (
         <div className={styles.productList}>
-          <img className={styles.iconImage} src={images.ingredients[item.image]} />
+          <img className={styles.iconImage} src={item.image} />
           <div className={styles.textArea}>
+            <p className={styles.titleText}>{item.title}</p>
             <p>{parse(item.text)}</p>
           </div>
         </div>
