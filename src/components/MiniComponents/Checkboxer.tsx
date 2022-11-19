1191d0fb7dedea3a6123821a6114242884e77d4c
Daniel Anca <steptu94@gmail.com>
2022-11-19 23:48:17 +0200

    Change FinishOrder checkboxes.

---
+++ src/components/MiniComponents/Checkboxer.tsx
@@ -0,0 +1,35 @@
+import React, { useCallback, useEffect, useState } from "react";
+import styles from "./Checkboxer.module.scss";
+
+interface InputProps {
+  onSwitchEnabled: (value: boolean, optionChoosed?: string) => void;
+  enabled?: boolean;
+}
+
+const Checkboxer = ({ enabled, onSwitchEnabled }: InputProps) => {
+  const [isChecked, setIsChecked] = useState<boolean>(enabled !== undefined ? enabled : false);
+
+  const onChangeState = useCallback(() => {
+    onSwitchEnabled(!isChecked);
+    setIsChecked(!isChecked);
+  }, [isChecked]);
+
+  useEffect(() => {
+    enabled !== undefined && onSwitchEnabled(isChecked);
+  }, [enabled]);
+
+  return (
+    <label className={styles.labelRow}>
+      <input type={"checkbox"} onChange={onChangeState}></input>
+      <path
+        className={`${styles.checkbox} ${isChecked ? styles.checkboxactive : ""}`}
+        aria-hidden="true"
+        viewBox="0 0 15 11"
+        fill="none"
+      />
+      <path d="M1 4.5L5 9L14 1" strokeWidth="2" stroke={isChecked ? "#fff" : "none"} />
+    </label>
+  );
+};
+
+export default Checkboxer;
