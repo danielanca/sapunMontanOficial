5cf484bff0fb98ad46d7677edd7ec0420d5f30a7
Daniel Anca <steptu94@gmail.com>
2022-11-21 11:41:04 +0200

    no message

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -375,7 +375,7 @@ export const sendEmail = functions.https.onRequest((request, response) => {
                         <td class="es-m-p0r" valign="top" align="center" style="padding:0;Margin:0;width:560px"> 
                          <table cellpadding="0" cellspacing="0" width="100%" role="presentation" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px"> 
                            <tr> 
-                            <td align="center" style="padding:0;Margin:0;padding-bottom:10px;font-size:0px"><img src="https://firebasestorage.googleapis.com/v0/b/sapunmontan.appspot.com/o/logo%2Fmontanair.png?alt=media&token=f42ebf81-1205-44d2-806b-7130100adda7" alt="Logo" style="display:block;border:0;outline:none;text-decoration:none;-ms-interpolation-mode:bicubic;font-size:12px" width="200" title="Logo"></td> 
+                            <td align="center" style="padding:0;Margin:0;padding-bottom:10px;font-size:0px"><img src="https://firebasestorage.googleapis.com/v0/b/diniubire-89ce0.appspot.com/o/emailTemplate%2FdinIubireLogoBro.png?alt=media&token=f9fe0cb8-7f70-4c1b-bd71-cc751f796d6e" alt="Logo" style="display:block;border:0;outline:none;text-decoration:none;-ms-interpolation-mode:bicubic;font-size:12px" width="200" title="Logo"></td> 
                            </tr> 
                          </table></td> 
                        </tr> 
@@ -758,7 +758,7 @@ export const sendEmail = functions.https.onRequest((request, response) => {
                           <td class="es-m-p0r" valign="top" align="center" style="padding:0;Margin:0;width:560px"> 
                            <table cellpadding="0" cellspacing="0" width="100%" role="presentation" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px"> 
                              <tr> 
-                              <td align="center" style="padding:0;Margin:0;padding-bottom:10px;font-size:0px"><img src="https://firebasestorage.googleapis.com/v0/b/sapunmontan.appspot.com/o/logo%2Fmontanair.png?alt=media&token=f42ebf81-1205-44d2-806b-7130100adda7" alt="Logo" style="display:block;border:0;outline:none;text-decoration:none;-ms-interpolation-mode:bicubic;font-size:12px" width="200" title="Logo"></td> 
+                              <td align="center" style="padding:0;Margin:0;padding-bottom:10px;font-size:0px"><img src="https://firebasestorage.googleapis.com/v0/b/diniubire-89ce0.appspot.com/o/emailTemplate%2FdinIubireLogoBro.png?alt=media&token=f9fe0cb8-7f70-4c1b-bd71-cc751f796d6e" alt="Logo" style="display:block;border:0;outline:none;text-decoration:none;-ms-interpolation-mode:bicubic;font-size:12px" width="200" title="Logo"></td> 
                              </tr> 
                            </table></td> 
                          </tr> 
