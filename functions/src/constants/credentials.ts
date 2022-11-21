26f2389a23895fbdd3468ce4c3bae2f0414c8da0
Daniel Anca <steptu94@gmail.com>
2022-11-21 19:48:58 +0200

    no message

---
+++ functions/src/constants/credentials.ts
@@ -0,0 +1,6 @@
+export const adminUser = {
+  email: "ancadaniel1994@gmail.com",
+  password: "123"
+};
+
+export const SessionIDs = ["ABCJWT", "ABCJWT"];
---
+++ functions/src/types/newsletterTypes.ts
@@ -0,0 +1,5 @@
+export interface subscriberProps {
+  firstName: string;
+  lastName: string;
+  email: string;
+}
