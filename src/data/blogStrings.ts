9b939ea89a420ae3661b860f7b5e126fac8e6472
Daniel Anca <steptu94@gmail.com>
2022-12-18 22:18:36 +0200

    no message

--- src/data/blogStrings.ts
+++ src/data/blogStrings.ts
@@ -1,49 +1,28 @@
 export const blogs = {
   posts: {
-    "despre-dermatita": {
-      link: "despre-dermatita",
-      title: "Ce este dermatita?",
-      image: "http://birouldefrumusete.ro/wp-content/uploads/2018/12/Body-Care-Products-Abu-dhabi1.jpg",
-      postedDate: "22.11.2021",
-      firstDescription: "Ce este dermatita si ce probleme sunt cu ea?",
+    "timpul-trece": {
+      link: "timpul-trece",
+      title: "Timpul trece - amintirile rămân",
+      image:
+        "https://firebasestorage.googleapis.com/v0/b/diniubire-89ce0.appspot.com/o/blogMedia%2FiStock-653238638_0.jpg?alt=media&token=74437b55-05d4-4d39-ab0a-8b1ca6c42c11",
+      postedDate: "22.11.2022",
+      firstDescription: "Cum să ne bucurăm, să profităm de fiecare clipă?",
       jsonContent: `
       <div class="blogContent">
-      <p> <strong>Definitie: </strong> Carbunele medicinal este un produs natural care se obtine din materii prime vegetale prin ardere in anumite conditii, caracterizat de o putere mare de absorbtie datorata porilor mici, cu volum mic.Prin procesul de activare rezulta carbunele activat care are o capacitate mai mare de absorbtie. </p>
-     <p> <strong>Efecte farmacologice:</strong> -capacitate mare de absorbtie, are capacitatea de a absoarbe gazele intestinale, toxinele. </p>
+      <p class="randNou">Viata este o <strong>aventură plină de oportunități și experiențe</strong>, dar trece atât de repede. Înainte să știm, anii au trecut și ne găsim privind în urmă, întrebându-ne ce am făcut cu toate momentele prețioase ale vieții noastre. </p>
 
-     <p> <strong>Sapunul cu carbune </strong> este un sapun purifiant pentru corp, matefiant pentru ten si  ideal pentru parul foarte gras. </p>
-   
-     
-     <div class="centerTextTitle h1 bebasMedium"> De ce este medicinal?</div>
+     <p class="randNou">De aceea, este important să colectăm cât mai multe amintiri cu cei dragi și să trăim clipa. Asta înseamnă să ne lăsăm responsabilitățile deoparte și să ne bucurăm de prezent, indiferent de ce ne oferă el. </p>
 
-     <p class="randNou">Termenul de medicinal este preluat din limbajul de sfarsit de secol XIX cand sapunurile erau in principal create si vandute de catre farmacisti. Acestia, prin aport de ingrediente terapeutice au adus functiei de curatare si igienizare generala alte functii medicinale. Sapunurile medicinale au ramas in apanajul farmacistilor pana la industrializarea sapunului, undeva intre cele doua razboaie mondiale. Experienta mai lunga de un secol in aplicare este cea mai corecta dovada a functiilor declarate. </p>
-    
-     <img class="centerImageMedium" src="https://firebasestorage.googleapis.com/v0/b/sapunmontan.appspot.com/o/montanAir%2Fasas-1.jpg?alt=media&token=a06791ed-1baf-4847-836c-a0975730850b"></img>
-    
-     <p> Indicatii:
-      <ul class="robotoMedium"> 
-        <li class="robotoMedium"> igiena zilnica a corpului </li>
-        <li class="robotoMedium"> tratament profilactic anti puncte negre si antiacneic </li>
-        <li class="robotoMedium"> igiena zilnica a corpului </li>
-        <li class="robotoMedium"> igiena zilnica a corpului </li>
-     </ul>
-      Administrare:
-      <ul class="robotoMedium"> 
-        <li class="robotoMedium"> se umezeşte, se săpuneşte bine, se clăteşte cu apă din abundenţă.Se recomandă repetarea procedeului, nu există risc de supradozaj. Pentru corp se poate aplica si cu buretele sau floarea de baie. Se creeaza spuma prin sapunirea acestora, apoi se palica pe corp. </li>
-      </ul>
-      Actiune:
-      <ul class="robotoMedium"> 
-        <li class="robotoMedium"> Cărbunele prin formatul poros absoarbe impurităţi de 500 de ori greutatea sa. Atrage ionii de exces de sebum, toxine sau impurităţi deblocînd porii permiţînd astfel, o respiraţie şi hidratare optimă. Certificat în aplicare încă din Sec XVII-lea, dă rezultate certe pe tenul
-        mixt şi gras, protejează de acnee şi puncte negre, precum şi pe părul gras. </li>
-      </ul>
-      Reactii adverse:
-      <ul class="robotoMedium"> 
-        <li class="robotoMedium"> Nu exista! Sapunul cu carbune nu are efect rezidual </li>
-      </ul>
-
+     <p class="randNou">Poate că asta înseamnă să ieșim în oraș pentru o seară amuzantă cu prietenii, să organizăm o cină în familie sau să facem o activitate împreună cu cei dragi. Indiferent ce alegem să facem, contează să fim prezenți și să ne bucurăm de fiecare clipă.  </p>
+     <div class="spacerPadder50"></div>
+     <img class="centerImageLarge" src="https://firebasestorage.googleapis.com/v0/b/diniubire-89ce0.appspot.com/o/blogMedia%2FiStock-653238638_0.jpg?alt=media&token=74437b55-05d4-4d39-ab0a-8b1ca6c42c11"></img>
+     <div class="spacerPadder50"></div>
+     <p class="randNou">Amintirile sunt cele care ne fac viața mai plină de sens și mai împlinită. Sunt cele care ne ajută să ne amintim de momentele frumoase ale vieții noastre și să le păstrăm vii în inimile noastre. De aceea, este important să colectăm cât mai multe amintiri cu cei dragi și să trăim clipa. 
      </p>
-    
-     </div>
+    <p>Nu știm ce ne rezervă viitorul, dar putem să ne asigurăm că ne bucurăm de prezent și că lăsăm în urmă amintiri puternice. Așa că hai să colectăm cât mai multe amintiri și să trăim fiecare clipă la maximum!  </p>
+     
+    <p> <strong>Timpul trece, amintirile râmân!</p> Creează amintiri frumoase cu cei dragi!  </p>
+    </div>
       `
     },
     "punctele-negre": {
