cb52851a4d2aacce20f56589f5154020c314571c
Daniel Anca <steptu94@gmail.com>
2023-02-11 02:23:44 +0200

    Added useScrollSense and implement on FAQ Block

--- src/components/ReviewField.tsx
+++ src/components/ReviewField.tsx
@@ -6,30 +6,19 @@ import styles from "./ReviewField.module.scss";
 import { ReviewsProps } from "../utils/ReviewsTypes";
 
 const ReviewField = ({ productData, productIdentification, allReviews }: ReviewsProps) => {
-  let reviewsList = Object.values(productData);
-
   return (
     <div className={styles.contain}>
-      {
-        // productData != null && reviewsList.length >= 2 ? (
-        //   <div className={styles.reviewContainer}>
-        //     {reviewsList.map((item) => (
-        //       <ReviewCard key={uniqueId()} testimonials={item} />
-        //     ))}
-        //   </div>
-        // ) :
-        productData != null && allReviews.length >= 1 ? (
-          <div className={styles.reviewContainer}>
-            {allReviews.map((item) => (
-              <ReviewCard key={uniqueId()} testimonials={item} />
-            ))}
-          </div>
-        ) : (
-          <div className={styles.noReviewsComponent}>
-            <h2>{"Nu exista recenzii"}</h2>
-          </div>
-        )
-      }
+      {productData != null && allReviews.length >= 1 ? (
+        <div className={styles.reviewContainer}>
+          {allReviews.map((item) => (
+            <ReviewCard key={uniqueId()} testimonialItem={item} />
+          ))}
+        </div>
+      ) : (
+        <div className={styles.noReviewsComponent}>
+          <h2>{"Nu exista recenzii"}</h2>
+        </div>
+      )}
 
       <AddReview productID={productIdentification} />
     </div>
